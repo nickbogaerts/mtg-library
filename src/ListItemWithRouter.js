8f64086699ae0af2915363233c24605bedcd51db
Nicholas Bogaerts <nicholas.bogaerts@mirumagency.com>
2017-12-18 17:05:13 +0000

    Cleared error from ListItemWithRouter component

--- src/ListItemWithRouter.js
+++ src/ListItemWithRouter.js
@@ -1,4 +1,7 @@
+import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { ListItem } from 'material-ui/List'
 
-export default withRouter(ListItem)
+export default withRouter(({ staticContext, ...props }) => {
+  return <ListItem {...props} />
+})
